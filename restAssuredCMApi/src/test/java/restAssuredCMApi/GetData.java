c8afebec86a5474dcdefb9458d7588b926042b90
JSO7296 <JSO7296@PARAJSO7296VDI.hca.corpad.net>
2019-08-14 22:59:27 -0400

    second

--- restAssuredCMApi/src/test/java/restAssuredCMApi/GetData.java
+++ restAssuredCMApi/src/test/java/restAssuredCMApi/GetData.java
@@ -7,6 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import io.restassured.authentication.FormAuthConfig;
 import io.restassured.response.Response;
 
 
@@ -22,8 +23,41 @@ public class GetData {
 		Response resp = given()
 				  .baseUri("http://xrpswqwebchg01b")
 				  .basePath("/")
-				  .cookie("ASP.NET_SessionId", "rj3yv5x0bt5niybkjkkuegba")
+				  .cookie("ASP.NET_SessionId", "d0rby0l2kpjsliwgzb540vg3")
 				  .get("/");
+		
+		//given().auth().form("HCA\\JSO7296", "Welcome2#").expect().statusCode(200).when().get("http://xrpswqwebchg01b");
+		
+		//given().auth().form("HCA\\JSO7296", "Welcome2#", FormAuthConfig.springSecurity()).expect().statusCode(200).when().get("http://xrpswqwebchg01b");
+
+		//Response resp =  get("http://xrpswqwebchg01b/Report/KeyingCompletedSOXReport").cookie();
+		
+			
+		int code = resp.getStatusCode();
+		
+		System.out.println("Status code is : " + code);
+		
+		
+	
+		String data = resp.asString();
+        System.out.println("Response received is :: " +data);
+        
+        Assert.assertEquals(code, 200);
+		
+		}
+	
+	@Test
+	public void postTest() {
+		
+		Response resp = given()
+				  .baseUri("http://xrpswqwebchg01b")
+				  .basePath("/")
+				  .cookie("ASP.NET_SessionId", "d0rby0l2kpjsliwgzb540vg3")
+				  .post("/Home/GetHomeDataTest");
+		
+		//given().auth().form("HCA\\JSO7296", "Welcome2#").expect().statusCode(200).when().get("http://xrpswqwebchg01b");
+		
+		//given().auth().form("HCA\\JSO7296", "Welcome2#", FormAuthConfig.springSecurity()).expect().statusCode(200).when().get("http://xrpswqwebchg01b");
 
 		//Response resp =  get("http://xrpswqwebchg01b/Report/KeyingCompletedSOXReport").cookie();
 		
@@ -41,3 +75,4 @@ public class GetData {
 		
 		}
 }
+
